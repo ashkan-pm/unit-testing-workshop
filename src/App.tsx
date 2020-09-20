8a875f21432cbc7d5b8921fb2a172cfb5fc85694
Ashkan Pourghasem <ashkan.pourghasem@gmail.com>
2020-09-21 01:55:31 +0400

    Remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import { Button } from "antd";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button>Learn React</Button>
-      </header>
+      <Button>Learn React</Button>
     </div>
   );
 }
