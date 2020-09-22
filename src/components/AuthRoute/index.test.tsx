68ba8804b69719cfb1ddcd8ea11b78f01a78e44a
Ashkan Pourghasem <ashkan.pourghasem@gmail.com>
2020-09-22 23:47:26 +0400

    Improve AuthRoute tests

--- src/components/AuthRoute/index.test.tsx
+++ src/components/AuthRoute/index.test.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Routes } from 'react-router-dom';
+import { Navigate, Routes } from 'react-router-dom';
 import { renderWithRouter, screen } from 'test-utils';
+import { getToken } from 'helpers/utils';
 import AuthRoute from './index';
 
-const TestComponent = ({ text }: { text: string }) => <span>{text}</span>;
+jest.mock('helpers/utils', () => ({
+  getToken: jest.fn(() => null)
+}));
+const mockGetToken = getToken as jest.MockedFunction<typeof getToken>;
 
-test('AuthRoute renders', () => {
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  Navigate: jest.fn(() => null)
+}));
+const MockNavigate = Navigate as jest.MockedFunction<typeof Navigate>;
+
+const TestComponent = ({ text }: { text?: string }) => <span>{text}</span>;
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+test('Renders children if token does not exist', () => {
   const path = '/login';
   const text = 'Test';
+  mockGetToken.mockReturnValue(null);
 
   renderWithRouter(
     <Routes>
@@ -15,6 +32,20 @@ test('AuthRoute renders', () => {
     </Routes>,
     { initialEntries: [path] }
   );
-
+  expect(mockGetToken).toHaveBeenCalledTimes(1);
   expect(screen.getByText(text)).toBeInTheDocument();
+});
+
+test('Redirects to / if token exists', () => {
+  const path = '/login';
+  mockGetToken.mockReturnValue('token');
+
+  renderWithRouter(
+    <Routes>
+      <AuthRoute path={path} element={<TestComponent />} />
+    </Routes>,
+    { initialEntries: [path] }
+  );
+  expect(mockGetToken).toHaveBeenCalledTimes(1);
+  expect(MockNavigate).toHaveBeenCalledWith({ to: '/', replace: true }, {});
 });
